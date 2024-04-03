1f66af897bd637f6bb848bdc8fc880fee12e39fc
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-04-03 12:06:47 +0000

    Add three odds to game

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+import random
   
 def name_player():
   """
@@ -7,48 +7,84 @@ def name_player():
   name = input('Choose Name:\n')
   return name
 
-def select_move():
-    while True:
 
-        player = input(f'select your move {name} \n>>>>  "R" or "P" or "S": \n')
 
-        return player
+def select_move_player():
+  """
+  the user should add the input here, which it sould be R or P or S in capital letter
+  """
+  player = input(f'select your move {name} \n>>>>  "R" or "P" or "S": \n')
+  return player
+
+
+def select_move_computer():
+  """
+  the computer will choose randomlly from the list
+  """
+  list=["R","P","S"]
+
+  sel = random.choice(list) 
+  print(sel)
+  return sel
         
+
 def vaild_move(validation):
 
   """
   I raised unvaild error here if the user throw anything not P or R or S
   """
-  try:
-    if  validation != str("P"):
-      raise ValueError(
-        f'you should enter either "R" or "P" or "S" in capital letter.\n'
-         )
+  
 
-  except ValueError as e:
-    print(f"incorrect move:{e}, please try agian.\n")
-    return False
-  return True
 
-  try:
-    if  validation != str("R"):
-      raise ValueError(
-        f'you should enter either "R" or "P" or "S" in capital letter.\n'
-         )
-  except ValueError as e:
-    print(f"incorrect move:{e}, please try agian.\n")
-    return False
-  return True
 
-  try:
-    if  validation != str("S"):
-      raise ValueError(
-        f'you should enter either "R" or "P" or "S" in  capital letter.\n'
-         )
-  except ValueError as e:
-    print(f"incorrect move:{e}, please try agian.\n")
-    return False
-  return True
+def results(move_player, move_computer):
+  """ there is three odds for each throw"""
+  
+  if move_player == "R":
+    if move_computer == "R":
+      print ("tied")
+    elif move_computer == "S":
+      print ("winner")
+    elif move_computer == "R":
+      print ("lost")
+
+  elif move_player == "P":
+    if move_computer == "P":
+      print ("tied")
+    elif move_computer == "R":
+      print ("winner")
+    elif move_computer == "S":
+      print ("lost")
+
+  elif move_player == "S":
+    if move_computer == "S":
+      print ("tied")
+    elif move_computer == "P":
+      print ("winner")
+    elif move_computer == "R":
+      print ("lost")
+  else:
+    print ("somthing went wrong")
+
+"""def game_over():
+  if move_player== "winner + winner":
+    print ("won")
+  elif move_player == "winner + teid":
+    print ("tied")
+  else:
+    print ("game over, sorry you lost")
+    exit ()"""
+
+
+
+  
+
+
+print ("in this Game, we use R for Rock, P for Paper and S for scissors\n")
+
+print ("The battle started\n")
+
+
 
 
 print ("in this Game, we use R for Rock, P for Paper and S for scissors\n")
@@ -56,121 +92,17 @@ print ("in this Game, we use R for Rock, P for Paper and S for scissors\n")
 print ("The battle started\n")
 
 name = name_player()
-move =select_move()
-validations = vaild_move(move)
-      
-    
-    
-         
-       
 
-"""
-  if p1r == "R":
-    if p2r == "R":
-      print ("tied")
-    elif p2r == "S":
-      print ("winner")
-    elif p2r == "P":
-      print ("lost")
-    else:
-      print ("somthing went wrong")
-  elif p1r == "P":
-    if p2r == "P":
-      print ("tied")
-    elif p2r == "S":
-      print ("lost")
-    elif p2r == "R":
-      print ("winner")
-    else:
-      print ("somthing went wrong")
-  elif p1r == "S":
-    if p2r == "S":
-      print ("tied")
-    elif p2r == "P":
-      print ("winer")
-    elif p2r == "R":
-        print ("lost")
-    else:
-       print ("somthing went wrong")
-  break
-while True:
-  print ("select your move")
-  print()
-  p1r = input ("p1 > ")
-  print ()
-  p2r = input ("pc > ")
-  print ()
-  if p1r == "R":
-    if p2r == "R":
-      print ("tied")
-    elif p2r == "S":
-      print ("winner")
-    elif p2r == "P":
-      print ("lost")
-    else:
-      print ("somthing went wrong")
-  elif p1r == "P":
-    if p2r == "P":
-      print ("tied")
-    elif p2r == "S":
-      print ("lost")
-    elif p2r == "R":
-      print ("winner")
-    else:
-      print ("somthing went wrong")
-  elif p1r == "S":
-    if p2r == "S":
-      print ("tied")
-    elif p2r == "P":
-      print ("winer")
-    elif p2r == "R":
-        print ("lost")
-    else:
-       print ("somthing went wrong")
-  break
-while True:
-  print ("select your move")
-  print()
-  p1r = input ("p1 > ")
-  print ()
-  p2r = input ("pc > ")
-  print ()
-  if p1r == "R":
-    if p2r == "R":
-      print ("tied")
-    elif p2r == "S":
-      print ("winner")
-    elif p2r == "P":
-      print ("lost")
-    else:
-      print ("somthing went wrong")
-  elif p1r == "P":
-    if p2r == "P":
-      print ("tied")
-    elif p2r == "S":
-      print ("lost")
-    elif p2r == "R":
-      print ("winner")
-    else:
-      print ("somthing went wrong")
-  elif p1r == "S":
-    if p2r == "S":
-      print ("tied")
-    elif p2r == "P":
-      print ("winer")
-    elif p2r == "R":
-        print ("lost")
-    else:
-       print ("somthing went wrong")
-  break
-if p1r == "winner + winner":
-  print ("won")
-elif p1r == "winner + teid":
-  print ("tied")
-else:
-  print ("game over")
-  exit ()
-"""
+def main():
+  player = select_move_player()
+  computer = select_move_computer()
+  result = results(player, computer)
+  #game_overs = game_over(result)
+
+
+main()
+
+
 
 
 
