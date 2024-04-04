9f3f87dae3190918e4c1c9e992b2124e864d3173
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-04-04 09:07:18 +0000

    Add scores plate

--- run.py
+++ run.py
@@ -7,71 +7,60 @@ def select_move():
   which it sould be R or P or S in capital letter,
   then the computer will choose randomlly from the list
   """
-  over = False
-  
-  while not over:
-    list=["R","P","S"]
-    
-    player = input(f'select your move {name} \n>>>>  "R" or "P" or "S": \n')
-    player = player.upper()
-    
-    computer = random.choice(list)
-    print(computer)
-    
-    results(player,computer)
-
-  return  results
-
-def results(move_player, move_computer):
-  """ there is three odds for each throw"""
   win = 0
   lose = 0
-  score = print(f'you got {win} and computer got {lose}')
+  
+  while True:
+    list=["R","P","S"]
+    
+    move_player  = input(f'select your move {name} \n>>>>  "R" or "P" or "S": \n')
+    move_player  = move_player .upper()
+    
+    move_computer = random.choice(list)
+    print(move_computer)
 
-  if move_player == "R":
-    if move_computer == "R":
-      print ("tied")
-      return score
-      return 0
-    elif move_computer == "S":
-      print ("winner")
-      return score
-      return win + 1
-    elif move_computer == "R":
-      print ("lost")
-      return score
-      return lose  + 1
+    if move_player == "R":
+      if move_computer == "R":
+        print ("tied")
+      elif move_computer == "S":
+        print ("winner")
+        win += 1
+      elif move_computer == "R":
+        print ("lost")
+        lose += 1
 
-  elif move_player == "P":
-    if move_computer == "P":
-      print ("tied")
-      return score
-      return 0
-    elif move_computer == "R":
-      print ("winner")
-      return score
-      return win + 1
-    elif move_computer == "S":
-      print ("lost")
-      return score
-      return lose  +1
+    elif move_player == "P":
+      if move_computer == "P":
+        print ("tied")
+      elif move_computer == "R":
+        print ("winner")
+        win += 1
+      elif move_computer == "S":
+        print ("lost")
+        lose += 1
 
-  elif move_player == "S":
-    if move_computer == "S":
-      print ("tied")
-      return score
-      return 0
-    elif move_computer == "P":
-      print ("winner")
-      return score
-      return win + 1
-    elif move_computer == "R":
-      print ("lost")
-      return score
-      return lose +1
-  else:
-    print(f'{name}, you enterd incorrect input/n')
-    print(f'please {name} choose one of "R" or "P" or "S"')
+    elif move_player == "S":
+      if move_computer == "S":
+        print ("tied")
+
+      elif move_computer == "P":
+        print ("winner")
+        win += 1
+      elif move_computer == "R":
+        print ("lost")
+        lose += 1
+        
+    else:
+      print(f'{name}, you enterd incorrect input/n')
+      print(f'please {name} choose one of "R" or "P" or "S"')
+
+    print(f'you got {win} and computer got {lose}')
+
+#def results(move_player, move_computer):
+  """ there is three odds for each throw"""
+
+
+
 
   
 
